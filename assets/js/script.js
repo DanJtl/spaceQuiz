d259485d56ba9864c3e991aa1a92f2e465e3e99e
DanJtl <daniel_junttila@live.se>
2022-01-05 10:40:10 +0000

    Add question structure in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+let questions = [
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correctAnswer: ""
+    },
+];
+
+
+
+
+
+let nextQuestion = document.getElementById("next-question");
+
+nextQuestion.addEventListener("click", getNextQuestion);
+
+function getNextQuestion() {
+    console.log("Next Question Coming Up!")
+}
